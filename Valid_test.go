f01e2ca58dad65c3720515ecc3009f5015576cba
Timofey Veretnov <tsuveret@gmail.com>
2022-04-04 23:39:00 +0700

    final testing

--- Valid_test.go
+++ Valid_test.go
@@ -1,67 +1,95 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestVal(t *testing.T) {
-	if ValidBraces("()") != true {
-		t.Errorf("incorrect sequence processing %v", "()")
-	}
-	if ValidBraces("[]") != true {
-		t.Errorf("incorrect sequence processing %v", "[]")
-	}
-	if ValidBraces("{}") != true {
-		t.Errorf("incorrect sequence processing %v", "{}")
-	}
-	if ValidBraces("{}()[]") != true {
-		t.Errorf("incorrect sequence processing %v", "{}()[]")
-	}
-	if ValidBraces("([{}])") != true {
-		t.Errorf("incorrect sequence processing %v", "([{}])")
-	}
-	if ValidBraces("{}({})[]") != true {
-		t.Errorf("incorrect sequence processing %v", "{}({})[]")
-	}
-	if ValidBraces("(({{[[]]}}))") != true {
-		t.Errorf("incorrect sequence processing %v", "(({{[[]]}}))")
-	}
-	if ValidBraces("(({[]{()}}[{(())}[]]))") != true {
-		t.Errorf("incorrect sequence processing %v", "(({[]{()}}[{(())}[]]))")
-	}
-	if ValidBraces("{[]}(){()}") != true {
-		t.Errorf("incorrect sequence processing %v", "{[]}(){()}")
-	}
-	if ValidBraces("[{()()()}]") != true {
-		t.Errorf("incorrect sequence processing %v", "[{()()()}]")
-	}
-	if ValidBraces("(){") != false {
-		t.Errorf("incorrect sequence processing %v", "(){")
-	}
-	if ValidBraces("}{") != false {
-		t.Errorf("incorrect sequence processing %v", "}{")
-	}
-	if ValidBraces("[}") != false {
-		t.Errorf("incorrect sequence processing %v", "(){")
-	}
-	if ValidBraces("(}") != false {
-		t.Errorf("incorrect sequence processing %v", "(}")
-	}
-	if ValidBraces("[)") != false {
-		t.Errorf("incorrect sequence processing %v", "[)")
-	}
-	if ValidBraces("([{)]}") != false {
-		t.Errorf("incorrect sequence processing %v", "([{)]}")
-	}
-	if ValidBraces("(((({{}})[]))") != false {
-		t.Errorf("incorrect sequence processing %v", "(((({{}})[]))")
-	}
-	if ValidBraces("()(){}[(])") != false {
-		t.Errorf("incorrect sequence processing %v", "()(){}[(])")
-	}
-	if ValidBraces("{(})") != false {
-		t.Errorf("incorrect sequence processing %v", "{(})")
-	}
-	if ValidBraces(")(}{[]") != false {
-		t.Errorf("incorrect sequence processing %v", ")(}{[]")
-	}
+	t.Run("A=positiveTest", func(t *testing.T) {
+		if ValidBraces("()") != true {
+			assert.Equal(t, ValidBraces("()"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "()")
+		}
+
+		if ValidBraces("[]") != true {
+			assert.Equal(t, ValidBraces("[]"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "[]")
+		}
+		if ValidBraces("{}") != true {
+			assert.Equal(t, ValidBraces("{}"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "{}")
+		}
+		if ValidBraces("{}()[]") != true {
+			assert.Equal(t, ValidBraces("{}()[]"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "{}()[]")
+		}
+		if ValidBraces("([{}])") != true {
+			assert.Equal(t, ValidBraces("([{}])"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "([{}])")
+		}
+		if ValidBraces("{}({})[]") != true {
+			assert.Equal(t, ValidBraces("{}({})[]"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "{}({})[]")
+		}
+		if ValidBraces("(({{[[]]}}))") != true {
+			assert.Equal(t, ValidBraces("(({{[[]]}}))"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "(({{[[]]}}))")
+		}
+		if ValidBraces("(({[]{()}}[{(())}[]]))") != true {
+			assert.Equal(t, ValidBraces("(({[]{()}}[{(())}[]]))"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "(({[]{()}}[{(())}[]]))")
+		}
+		if ValidBraces("{[]}(){()}") != true {
+			assert.Equal(t, ValidBraces("{[]}(){()}"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "{[]}(){()}")
+		}
+		if ValidBraces("[{()()()}]") != true {
+			assert.Equal(t, ValidBraces("[{()()()}]"), true, "это правильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "[{()()()}]")
+		}
+	})
+	t.Run("A=positiveTest", func(t *testing.T) {
+		if ValidBraces("(){") != false {
+			assert.Equal(t, ValidBraces("(){"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "(){")
+		}
+		if ValidBraces("}{") != false {
+			assert.Equal(t, ValidBraces("}{"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "}{")
+		}
+		if ValidBraces("[}") != false {
+			assert.Equal(t, ValidBraces("[}"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "(){")
+		}
+		if ValidBraces("(}") != false {
+			assert.Equal(t, ValidBraces("(}"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "(}")
+		}
+		if ValidBraces("[)") != false {
+			assert.Equal(t, ValidBraces("[)"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "[)")
+		}
+		if ValidBraces("([{)]}") != false {
+			assert.Equal(t, ValidBraces("([{)]}"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "([{)]}")
+		}
+		if ValidBraces("(((({{}})[]))") != false {
+			assert.Equal(t, ValidBraces("(((({{}})[]))"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "(((({{}})[]))")
+		}
+		if ValidBraces("()(){}[(])") != false {
+			assert.Equal(t, ValidBraces("()(){}[(])"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "()(){}[(])")
+		}
+		if ValidBraces("{(})") != false {
+			assert.Equal(t, ValidBraces("{(})"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", "{(})")
+		}
+		if ValidBraces(")(}{[]") != false {
+			assert.Equal(t, ValidBraces(")(}{[]"), true, "это неправильная скобочная последовательность ")
+			t.Errorf("incorrect sequence processing %v", ")(}{[]")
+		}
+	})
 
 }
